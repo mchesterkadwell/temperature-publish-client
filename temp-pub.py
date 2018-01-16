a80bfba780a36d2b40134fcf1888b9569adcf001
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2018-01-16 11:58:54 +0000

    Get client var name right!

--- temp-pub.py
+++ temp-pub.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-import context
 import paho.mqtt.client as mqtt
 
-client = mqtt.Client()
-mqtt.connect("localhost")
+client = mqttc.Client()
+mqttc.connect("localhost")
 print("Publishing message to topic","paho/test")
 client.publish("paho/test","Test message!")
