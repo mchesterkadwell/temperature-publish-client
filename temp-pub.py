7a41ba67b5a29a9c64aba75f75743a12211ebefb
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2018-01-16 12:20:55 +0000

    Add message as argument to script

--- temp-pub.py
+++ temp-pub.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
+import argparse
 import paho.mqtt.client as mqtt
+
+def parse_args():
+    """Parse one argument from command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("message", help="Message to publish")
+    args = parser.parse_args()
+    return args.message
+
+message = parse_args()
 
 client = mqtt.Client()
 client.connect("localhost")
-print("Publishing message to topic","paho/test")
-client.publish("paho/test","Test message!")
+print("Publishing message to topic", "paho/test")
+client.publish("paho/test", message)
