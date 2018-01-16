8a24eb7785c447385cfa2d58e4d1b7b2c84d49c2
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2018-01-16 11:55:05 +0000

    Fix typo in mqtt

--- temp-pub.py
+++ temp-pub.py
@@ -3,6 +3,6 @@
 import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
-mqttc.connect("localhost")
+mqtt.connect("localhost")
 print("Publishing message to topic","paho/test")
 client.publish("paho/test","Test message!")
