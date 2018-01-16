90452371ab1fb64563f310bcdc9530afce92009e
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2018-01-16 23:11:25 +0000

    Publish different thermometers to different topics

--- temp-pub.py
+++ temp-pub.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 
 import sys
+import json
 
 import paho.mqtt.client as mqtt
 
-import config
+from config import *
 
-client = mqtt.Client()
-client.connect(config.host)
-print("Publishing message to topic", "paho/test")
+def on_publish(client, userdata, msg):
+    print("Publishing message %s to topic %s" % (str(msg), topic))
+
+client = mqtt.Client(CLIENT_NAME)
+client.on_publish = on_publish
+client.connect(MQTT_SERVER)
+
+last_message = ""
 for message in sys.stdin:
-    if not message.isspace():
-        client.publish("paho/test", message.rstrip())
+    if not message.isspace() and message != last_message:
+        last_message = message
+        parsed_message = json.loads(message)
+        thermometer_id = parsed_message['id']
+        thermometer_location = THERMOMETERS[thermometer_id]
+        topic = TOPIC_PREFIX + thermometer_location
+        client.publish(topic, message.rstrip())
--- template.config.py
+++ template.config.py
@@ -1,3 +1,15 @@
 #Copy as config.py and change values as needed
 
-host="localhost"
+MQTT_SERVER="localhost"
+CLIENT_NAME="rf_thermometers"
+
+TOPIC_PREFIX="house/thermometers/"
+
+#{id:location}
+#ID is reported by each thermometer
+#Location is used to create topic name
+THERMOMETERS={
+    1:"bedroom",
+    2:"kitchen",
+    3:"outside"
+}
